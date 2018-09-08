3ae5f9e991fe09dad79891d6b02c0d5b90cddd2f
Jacob Yoon <jacobyoon926@gmail.com>
2018-09-08 00:48:05 -0400

    Update

--- js/script.js
+++ js/script.js
@@ -1,50 +1,27 @@
  $(document).ready(function(){
 
-	/*/Parallax On Big Displays
+	//BG On Big Displays
 	if ( $(window).width() > 739) {
-		//var $navBar = $('.navBar');
 		var $win = $(window);
-		/*
-		//Fixed Navigation Bar
+		var $bg = $('.bg');
+		var $bg2 = $('.bgdarker');
+		var $section2 = $('.section2');
+
 		$win.on('scroll', function() {
-			var top = $win.scrollTop();
-			$navBar.css('top', '' + top + 'px');
-			console.log(top);
+			var top = $win.scrollTop()/-6000;
+			var top1 = $win.scrollTop()/1300;
+			var top2 = $win.scrollTop();
+			var offset = $('.space').offset().top;
+			var distance = (offset - top2);
+			$bg.css('transform', 'scale(' + (top + 1.3) + ')');
+			$bg2.css('opacity', top1);
+			$section2.css('transform', 'translateX(' + ((top2 - distance - 2500) * -1) + 'px)');
+			$section2.css('opacity', top1);
 		});
 
 
-		//Parallax circles
-		var $circle1 = $('.circle1');
-		var $circle2 = $('.circle2');
-		var $circle3 = $('.circle3');
-		var $circle4 = $('.circle4');
-		var $circle5 = $('.circle5');
 
-		$win.on('scroll', function() {
-<<<<<<< HEAD
-			var top1 = $win.scrollTop()/800;
-			var top2 = ($win.scrollTop() - 150);
-			var top3 = $win.scrollTop() + 50;
-			var top4 = $win.scrollTop() - 20;
-			$circles.css('opacity', (top1 * -1) + 1);
-			$circles.css('margin-bottom', (top2.toFixed(1) * -1) + 'px');
-			$circle1.css('margin-left', (top3.toFixed(1) * -1) + 'px');
-			$circle2.css('margin-left', (top4.toFixed(1)) + 'px');
-		});
-=======
-			var top1 = $win.scrollTop()/3;
-			var top2 = $win.scrollTop()/2.5;
-			var top3 = $win.scrollTop()/1.5;
-			var top4 = $win.scrollTop()/2;
-			var top5 = $win.scrollTop()/6;
-			$circle1.css('transform', 'translateY(' + top1.toFixed(1) + 'px)');
-			$circle2.css('transform', 'translateY(' + top2.toFixed(1) + 'px)');
-			$circle3.css('transform', 'translateY(' + top3.toFixed(1) + 'px)');
-			$circle4.css('transform', 'translateY(' + top4.toFixed(1) + 'px)');
-			$circle5.css('transform', 'translateY(' + top5.toFixed(1) + 'px)');
-		});*/
-
-	}*/
+	}
 
 	//Smooth Scrolling Between Links
 	$("a").on('click', function(event) {
@@ -58,19 +35,4 @@
 			});
 		}
 	});
-
-	/*Change header height on scroll
-	$(window).on("scroll", function () {
-		if ($(this).scrollTop() > 50) {
-      $(".header").css("background", "#070014");
-	   	$(".header").css("height", "100px");
-		}
-		else {
-	   	$(".header").css("background", "rgba(0,0,0,0)");
-      $(".header").css("height", "150px");
-		}
-	});
-	*/
-
-
 });
